56afecdf31928cd67bbc9a9dec69d1d3fa46be04
ljimenezt <ljimenezt@kio.tech>
2026-05-20 17:34:57 -0600

    Se convirtió variable de int a string

---
+++ int_to_str.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * We need a function that can transform a number (integer) into a string.
+ * What ways of achieving this do you know?
+ */
+
+function numberToString($num) {
+  return (string)$num;
+}
+
+var_dump(numberToString(100));
